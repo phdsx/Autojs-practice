6348e4b28731e9f34a211817e0affe55eb626111
phdsx <pp93314@163.com>
2021-03-15 13:48:32 +0800

    v0.3.7
    8

--- 能量雨.js
+++ 能量雨.js
@@ -1,8 +1,5 @@
-/*
-version : 0.2.4
-author  : phdsx
-*/
 auto();
+
 setScreenMetrics(1176, 2400);
 
 if(!requestScreenCapture()){
@@ -11,16 +8,25 @@ if(!requestScreenCapture()){
 }
 var width=device.width;
 var height=device.height;
-
+toast("将于5秒钟后开始点击");
 sleep(1000);
+toast("请切换至立即开始页面");
+sleep(3000);
+click(0.5*width,0.655*height);
+sleep(2800);
 var thread1=threads.start(function(){
     var x=500;
     var y=850;
+    var delta=100;
     while(true){
-        var point = findColorInRegion(captureScreen(), "#ff219300", 100,0.275*height, 0.9*width,250);
+        var time_start=new Date();
+        var point = findColorInRegion(captureScreen(), "#219300", 100,0.275*height, 0.9*width,250);
         if(point){
             x=point.x+20;
-            y=point.y+100;
+            var time_end=new Date();
+            delta=(time_end.getTime()-time_start.getTime())/30*100
+            
+            y=point.y+delta;
             press(x ,y,40);
             sleep(50);
         }else{
@@ -30,5 +36,5 @@ var thread1=threads.start(function(){
     }
 });
 
-sleep(15000);
+textContains("感谢你又为地球做出了贡献").waitFor();
 thread1.interrupt();
